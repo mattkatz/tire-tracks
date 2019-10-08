d4e110f7b81de9f6b8a2419db9d3e460f89adcc3
Matt Katz <git@morelightmorelight.com>
2019-10-07 22:22:59 -0400

    quick todo comments

--- tire_tracks/tire_tracks.py
+++ tire_tracks/tire_tracks.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+from pathlib import Path
+
+
+class TireTracks():
+    def __init__(self, args):
+        self.wheelpath = Path(args.wheel)
+        self.outdir = Path(args.outdir)
+
+    def impress(self):
+        # unzip the wheel to a temp directory
+        # create a docs directory
+        # run sphinx against the docs directory
