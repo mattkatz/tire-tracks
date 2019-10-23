a730c1f21f000d7d0c123b2a6770226244e7fdbf
Matt Katz <matthew.katz+git@morelightmorelight.com>
2019-10-23 17:19:42 -0400

    fix init

--- tests/test_tire_tracks.py
+++ tests/test_tire_tracks.py
@@ -6,20 +6,39 @@
 import pytest
 
 
-from tire_tracks import tire_tracks
+from tire_tracks.tire_tracks import TireTracks
+from zipfile import ZipFile
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def wheel():
+    """ return a zipfile like a wheel"""
+    td = TemporaryDirectory()
+    temp_path = Path(td.name)
+    z = ZipFile((temp_path / "test_wheel.whl"), "w")
+    for token in ["foo", "bar", "blah"]:
+        tokenp = temp_path / (token + ".py")
+        tokenp.write_text("pass")
+        z.write(tokenp)
+    z.close()
+    return Path(z.filename)
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
+def test_creates_working_dir_with_fake():
+    td = TemporaryDirectory()
+    temp_path = Path(td.name)
+    z = ZipFile((temp_path / "test_wheel.whl"), "w")
+    for token in ["foo", "bar", "blah"]:
+        tokenp = temp_path / (token + ".py")
+        tokenp.write_text("pass")
+        z.write(tokenp)
+    z.close()
+    wheel = Path(z.filename)
+    t = TireTracks(wheel, ".")
+    with t.create_working_dir(wheel) as wd:
+        assert wd.is_dir()
+    wd2 = t.create_working_dir(wheel)
+    assert wd2.is_dir()
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
--- tire_tracks/tire_tracks.py
+++ tire_tracks/tire_tracks.py
@@ -2,15 +2,38 @@
 
 """Main module."""
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from contextlib import contextmanager
+from zipfile import ZipFile
 
 
 class TireTracks():
-    def __init__(self, args):
-        self.wheelpath = Path(args.wheel)
-        self.outdir = Path(args.outdir)
+    def __init__(self, wheel, outdir):
+        self.wheelpath = Path(wheel)
+        self.outdir = Path(outdir)
 
     def impress(self):
         # unzip the wheel to a temp directory
         # create a docs directory
         # run sphinx against the docs directory
         pass
+
+    @contextmanager
+    def create_working_dir(self, wheel: Path) -> Path:
+        """Given a *wheel* Path, unzip to a temporary directory and return that working directory.
+
+        :param wheel:
+        :type wheel: Path
+        :rtype: Path
+        """
+
+        wheelzip = ZipFile(wheel)
+        # with a tempdir
+        tempd = TemporaryDirectory()
+        # unzip the wheel into tempdir
+        # TODO: optimize this to only extract what we will need
+        wheelzip.extractall(tempd.name)
+        # yield for use as a Path
+        yield Path(tempd.name)
+        # let it get cleaned up
+        return Path(tempd.name)
