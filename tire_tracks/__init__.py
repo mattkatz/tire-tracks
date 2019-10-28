1a015ca06e58df8913bbf0e011263d57f6fc488b
Matt Katz <matthew.katz+git@morelightmorelight.com>
2019-10-27 21:37:49 -0400

    Black changes

--- tire_tracks/__init__.py
+++ tire_tracks/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for Tire Tracks."""
 
 __author__ = """Matt Katz"""
-__email__ = 'github@morelightmorelight.com'
-__version__ = '0.1.0'
+__email__ = "github@morelightmorelight.com"
+__version__ = "0.1.0"
